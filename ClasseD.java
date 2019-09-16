813f6d966ec86b9768f471dd199c3ca32eef229b
rafaelvr14 <rafael.fernandes@estudante.ufla.br>
2019-09-15 22:05:46 -0300

    Criação da classe D

---
+++ ClasseD.java
@@ -0,0 +1,10 @@
+public class D{
+    private Int D1;
+    private Int D2;
+    public void MD1(){
+        System.out.println("MD1");
+    }
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
