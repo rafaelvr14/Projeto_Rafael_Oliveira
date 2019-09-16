7f70359c30928c03dd2a7887b0dd7d626bc15ebc
rafaelvr14 <rafael.fernandes@estudante.ufla.br>
2019-09-15 21:12:01 -0300

    Testando

---
+++ Rafael_Oliveira_10A.java
@@ -0,0 +1,46 @@
+public class A {
+    private Int A1;
+    private Float A2;
+
+    public Int getA1() {
+        return A1;
+    }
+    public Int getA2() {
+        return A2;
+    }
+    public void setA1(Int A1) {
+        this.A1=A1;
+    }
+    public void setA2(Float A2) {
+        this.A2=A2;
+    }
+    public void MA1() {
+        System.out.println("MA1");
+    }
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
+public class B{
+    private Int B1;
+    private Float B2;
+    
+    public Int getB1() {
+        return B1;
+    }
+    public Float getB2() {
+        return B2;
+    }
+    public void setB1(Int B1) {
+        this.B1=B1;
+    }
+    public void setB2(Float B2) {
+        this.B2=B2;
+    }
+    public void MB1() {
+        System.out.println("MB1");
+    }
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
