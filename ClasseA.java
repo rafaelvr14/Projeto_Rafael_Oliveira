c9a626585d1cf899623da513b708004fecb9d2d0
rafaelvr14 <rafael.fernandes@estudante.ufla.br>
2019-09-15 21:36:06 -0300

    Teste 1.1

---
+++ ClasseA.java
@@ -0,0 +1,26 @@
+public class A {
+    private Int A1;
+    private Float A2;
+
+    public Int getA1() {
+        return A1;
+    }
+    public Int getA2() {
+        return A2;
+    }
+    public void setA1(Int A1) {
+        this.A1=A1;
+    }
+    public void setA2(Float A2) {
+        this.A2=A2;
+    }
+    public void MA1() {
+        System.out.println("MA1");
+    }
+    public void MA2() {
+        System.out.println("MA2");
+    }
+    public void MA3(){
+        System.out.println("Alteração a classe A partir do clone");
+    }
+}
--- ClasseB.java
+++ ClasseB.java
@@ -1,26 +1,3 @@
-public class A {
-    private Int A1;
-    private Float A2;
-
-    public Int getA1() {
-        return A1;
-    }
-    public Int getA2() {
-        return A2;
-    }
-    public void setA1(Int A1) {
-        this.A1=A1;
-    }
-    public void setA2(Float A2) {
-        this.A2=A2;
-    }
-    public void MA1() {
-        System.out.println("MA1");
-    }
-    public void MA2() {
-        System.out.println("MA2");
-    }
-}
 public class B{
     private Int B1;
     private Float B2;
